cb565b22b9757613be7ed3bb1d6af71c0c446aac
dwhddnjs <per1215@naver.com>
2024-09-30 23:34:08 +0900

    ✨ 라이브러리 테스트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { H1, Theme } from 'tamagui';
+import { H1, Paragraph, Theme, XStack, YStack } from 'tamagui';
 
-import { Button, Main } from '~/tamagui.config';
+import { Button, Container, Main } from '~/tamagui.config';
 
 export default function Tab() {
   return (
     <Main>
-      <Button>asdasd</Button>
-      <H1>sdasdsad</H1>
+      <Container>
+        <Button>asdasd</Button>
+        <H1>sdasdsad</H1>
+        <Paragraph>asdasd</Paragraph>
+        <YStack>
+          <XStack />
+        </YStack>
+      </Container>
     </Main>
   );
 }
